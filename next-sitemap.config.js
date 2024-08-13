67244460ac2355634469fb8087fa9ce543d8641a
Kiril Kamenov <kiril.kk2002@gmail.com>
2024-08-13 14:33:18 +0200

    Creando los archivos robot.txt y sitemap.xml para Google

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://putnapomosht-provadia.bg', // Reemplaza esto con la URL de tu sitio
+    generateRobotsTxt: true, // (Opcional) Genera un archivo robots.txt
+    // Opciones adicionales según sea necesario
+  }
+  
---
+++ next-sitemap.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: "https://putnapomosht-provadia.bg",
+  generateRobotsTxt: true,
+};
